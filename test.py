ff86facdd3005e27bf3b02e4e5803395d0f1be31
Valera <valera.glukhovtsev@gmail.com>
2019-02-05 15:00:47 +0500

    seleniumUnittest

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import unittest
+from selenium import webdriver
 
 class FirstTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
     def test_first(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
         print("DOCKERRRRRRRRRRRRRRRR")
+        driver.close()
 
 if __name__ == '__main__':
     unittest.main()
